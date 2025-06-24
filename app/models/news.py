91b843e6f96042b8d14db4c9753944ac2fbe4645
Alicly <137168801+Alicly@users.noreply.github.com>
2025-06-24 13:08:21 +0300

    Add files via upload

--- app/models/news.py
+++ app/models/news.py
@@ -10,6 +10,9 @@ class News(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow)
     image_url = db.Column(db.String(200))
     is_published = db.Column(db.Boolean, default=True)
+    category = db.Column(db.String(50), default='general')
+    author = db.Column(db.String(100))
+    views = db.Column(db.Integer, default=0)
     
     def __repr__(self):
         return f'<News {self.title}>' 
